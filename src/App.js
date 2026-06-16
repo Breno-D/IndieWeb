27fbf83857ef5de834691284249f0920f7df7eaf
Breno D <brenord@hotmail.com.br>
2026-06-16 15:43:26 -0300

    left container organization and add package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './Components/Header/Header.js'
 import Navbar from './Components/Header/Navbar.js';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
   return (
@@ -8,9 +10,25 @@ function App() {
       <Header></Header>
       <Navbar></Navbar>
         <div class="mainContent">
-            <div class="left-content"></div>
+            <div class="left-content">
+              <div class="box"></div>
+              <div class="box"></div>
+              <div class="box"></div>
+              <div class="box"></div>
+            </div>
             <div class="middle-content"></div>
-            <div class="right-content"></div>
+            <div class="right-content">
+              <div class="calendar-div">
+                <Calendar />
+              </div>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+
+            </div>
         </div>
         <div class="footer"></div>
     </div>
