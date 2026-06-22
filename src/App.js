9f2a2f9552ccbf0385f3bd3572dafeb1b30f3cec
Breno D <brenord@hotmail.com.br>
2026-06-22 13:54:05 -0300

    bring orange to color pallete and add site introduction

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
             <div className="middle-content">
               <div className='box mainContent-box'>
                 <h3>Introdução do Site</h3>
-                <p></p>
+                <p>Bem vindo ao meu cantinho</p>
+                <p>Esse site surgiu majoritariamente após ser inspirado ao assistir <a className='text-link' href='https://www.youtube.com/watch?v=tkUgOT22F5s' target='blank'>A Web Revival: the Internet didn't die, you're just not on it</a>, esse vídeo me fez querer criar uma pagina tal qual as várias que eu visitava na minha infância perto dos anos 2000, e o rabbithole que eu cai de vários vídeos sobre IndieWeb depois só consolidaram essa minha vontade e me fizeram começar a produzir!</p>
+                <p>Como eu não sou a pessoa mais letrada sobre design, sou apenas um programador, o design desse site é apenas um agregado ou uma homenagens de alguns dos sites que visitei na jornada de criar esse meu cantinho :D</p>
               </div>
               <div className='box mainContent-box'>
                 <h3>Sobre Mim</h3>
