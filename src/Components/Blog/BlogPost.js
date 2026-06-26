e9da5fd2f9592d6e79a206affefb5288c1c467e9
Breno D <brenord@hotmail.com.br>
2026-06-26 08:10:10 -0300

    blog post with pth pictures

--- src/Components/Blog/BlogPost.js
+++ src/Components/Blog/BlogPost.js
@@ -3,7 +3,16 @@ export default function BlogPost({ post }) {
     <article className="blog-post box">
       <h2>{post.title}</h2>
       <time>{post.date}</time>
-      <p style={{ whiteSpace: "pre-line" }}>{post.content}</p>
+      {/* <p style={{ whiteSpace: "pre-line" }}>{post.content}</p> */}
+        {post.content.map((block, index) => {
+        if (block.type === "text") {
+          return <p key={index}>{block.value}</p>;
+        }
+        if (block.type === "image") {
+          return <img key={index} src={block.src} alt={block.alt} className="blog-inline-img" />;
+        }
+        return null;
+      })}
     </article>
   );
 }
--- src/posts-blog/post-001.js
+++ src/posts-blog/post-001.js
@@ -5,8 +5,12 @@ export default {
   title: "Meu primeir post",
   date: "2026-06-01",
   summary: "sem blog escrito aindar",
-  content: `
-    Ainda não temos bloggerino escrito
-  `,
+  content: [
+    { type: "text", value: "Primeiro parágrafo do post aqui." },
+    { type: "image", src: cover, alt: "Descrição da imagem" },
+    { type: "text", value: "Segundo parágrafo, depois da imagem." },
+    { type: "image", src: cover, alt: "Outra imagem" },
+    { type: "text", value: "Parágrafo final." },
+  ],
   cover
 };
