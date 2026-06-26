07be0538d912feb7eb07c91590264225580dc6af
Breno D <brenord@hotmail.com.br>
2026-06-26 09:45:39 -0300

    devtutorial intro

--- src/Pages/GameDevTutorial/GameDevTutorial.js
+++ src/Pages/GameDevTutorial/GameDevTutorial.js
@@ -15,6 +15,12 @@ function GameDevTutorial() {
             <Leftbar></Leftbar>
             <div className="middle-content">
               <div className='box mainContent-box gamedevbox'>
+                <h3>Introdução</h3>
+                <p>Em uma das empresas de desenvolvimento de jogos que trabalhei uma das visões da empresa era fomentar o cenário de desenvolvimento de jogos do Brasil, e eu peguei um pouco dessa visão para mim!</p>
+                <p>Abaixo você vai encontrar um tutorial (que vai ser mais atualizado conforme o tempo) de como começar a sua jornada de desenvolvimento de jogos.</p>
+                <p>Na primeira seção você vai encontrar um sumário que vai te levar ao conteúdo desejado na pagina, basta clicar para pular para onde quiser caso queira pular direito para algum conteúdo ou relembrar algo lá na frente em um momento de necessidade! :D</p>
+              </div>
+              <div className='box mainContent-box gamedevbox'>
                 <h3>Sumário</h3>
                 <a href='#testelink'><h4>Primeira Teste</h4></a>
                 <a href='#especifico'><h5>--Sublink</h5></a>
